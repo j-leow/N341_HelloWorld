02dd7d0bb9ac69e50beeaffd4e01f3865652d367
Jules Leow <jleow@iupui.edu>
2017-08-30 11:47:14 -0500

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,16 @@
+/**************************************
+ TITLE: helloWorld.js
+ AUTHOR: Jules Leow
+ CREATE DATE: 29 Aug 2017
+ PURPOSE: Create a script to show an alert popup
+ LAST MODIFIED ON: 29 Aug 2017
+ LAST MODIFIED BY: Jules Leow (JL)
+ MODIFICATION HISTORY:
+ Original Build
+***************************************/
+
+$(document).ready(function () {
+
+  alert("Ahoy matey! Pleased t' meet ye.");
+
+});
